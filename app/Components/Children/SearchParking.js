53f4c57720de6605e71ce017aaa24e9cdd4511c8
Mihir06121 <mp06121@gmail.com>
2020-03-28 14:32:18 +0530

    navbar logo removed/ logo added to body

--- app/Components/Children/SearchParking.js
+++ app/Components/Children/SearchParking.js
@@ -42,6 +42,9 @@ var SearchParking = React.createClass({
      return( 
        <div className="jumbotron">
          <div id="frontcontainer" className="container">
+           <div className="container-logo">
+              <img src="/assets/images/drive2park.png" style={{width:"200px", height:"200px",display:"block", margin:"auto"}}></img>
+           </div>
            <h2 className="text-center"> Find and Book Parking in seconds</h2>
            <form onSubmit={this.handleSubmit} className="form-inline" action="/action">
              <div className="form-group">
