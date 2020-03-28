d46dd442fcae691bedea0a3725db29fc401570cd
Mihir06121 <mp06121@gmail.com>
2020-03-28 15:41:30 +0530

    searchbar changed

--- app/Components/Children/SearchParking.js
+++ app/Components/Children/SearchParking.js
@@ -48,7 +48,7 @@ var SearchParking = React.createClass({
            <h2 className="text-center"> "<u>Find and Book Parking in seconds</u>"</h2>
            <form onSubmit={this.handleSubmit} className="form-inline" action="/action">
              <div className="form-group">
-               <label htmlFor="city" className="indexlable">City:</label>
+               <label htmlFor="city" className="indexlable"></label>
                <input type="text" value={this.state.city} onChange={this.handleChange} className="form-control" id="city" placeholder="Enter City Name" name="city"/>
              </div>
              <button type="submit" className="btn btn-primary btn-xl">Search</button>
