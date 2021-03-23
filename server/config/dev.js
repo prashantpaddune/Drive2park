c7c6da1ceab6eef6f5a93a504bd01e931a47141d
prashantpaddune <padadunep@gmail.com>
2021-03-23 18:05:44 +0530

    add test keys

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,9 +1,9 @@
 module.exports = {
   // Get DB_URI on mongo atlas, this one is just for example
-  DB_URI: '',
+  DB_URI: 'mongodb://localhost:27017/test',
   // Provide unique value
-  JWT_SECRET: '',
-  CLOUDINARY_NAME: '',
-  CLOUDINARY_KEY: '',
-  CLOUDINARY_SECRET: ''
+  JWT_SECRET: 'prashant',
+  CLOUDINARY_NAME: 'dyd0ckv4b',
+  CLOUDINARY_KEY: '748592412323862',
+  CLOUDINARY_SECRET: 'UhMVnfpYNyRpNg7CtVH3j-zIHOw'
 }
